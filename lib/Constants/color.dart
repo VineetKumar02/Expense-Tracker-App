01c0de4cd35164f2761f4bb2cc44737f9e79a0fd
vineetkumar02 <vineethero02@gmail.com>
2023-11-14 18:55:45 +0530

    first commit

---
+++ lib/Constants/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color moneyBoyPurple = Color(0xFF7A69EA);
+const Color moneyBoyPurpleLight = Color(0xFF9C91ED);
+// const Color primaryColor = Color(0xff368983);
+const Color primaryColor = Colors.blue;
